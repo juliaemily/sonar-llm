1808426f6768b4f90ea35992c86de9052787ddd9
Júlia Emily <83286441+juliaemily@users.noreply.github.com>
2025-04-30 21:45:50 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,21 +1,3 @@
 <?php
-
-// Exemplo de SQL Injection
-$user = $_GET['user'];
-$password = $_GET['pass'];
-$query = "SELECT * FROM users WHERE username = '$user' AND password = '$password'";
-$result = mysqli_query($conn, $query);
-
-// Variável não utilizada
-$debug = true;
-
-// Função grande sem coesão
-function processStuff($input) {
-    for ($i = 0; $i < 100; $i++) {
-        echo $i;
-    }
-    $json = json_decode($input);
-    file_put_contents("log.txt", $json->data);
-}
-
-?>
+$input = $_GET['x'];
+eval($input); // alerta clássico de vulnerabilidade
