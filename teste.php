30fb61286446520fd8d6fb6d57eec545c26e05de
juliaemily <juliaemily614@gmail.com>
2025-04-30 21:29:24 -0300

    Alterando arquivo php e incluindo arquivos workflow

--- teste.php
+++ teste.php
@@ -1,3 +1,21 @@
 <?php
-$input = $_GET['x'];
-eval($input); 
+
+// Exemplo de SQL Injection
+$user = $_GET['user'];
+$password = $_GET['pass'];
+$query = "SELECT * FROM users WHERE username = '$user' AND password = '$password'";
+$result = mysqli_query($conn, $query);
+
+// Variável não utilizada
+$debug = true;
+
+// Função grande sem coesão
+function processStuff($input) {
+    for ($i = 0; $i < 100; $i++) {
+        echo $i;
+    }
+    $json = json_decode($input);
+    file_put_contents("log.txt", $json->data);
+}
+
+?>
